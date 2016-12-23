f35e2def3235a0b1469b8b8c2f3fe225900a21d9
Asus <Asus@192.168.1.28>
2016-12-23 11:49:47 +0800

    Done

---
+++ src/test/Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Asus
+ */
+public class Test {
+      public static void main (String[] args){
+            int a=3, b=2, c=1;
+            if (a>c){
+                a = b = c = 1;
+
+                System.out.println(b);
+                          }
+}
+}
+
